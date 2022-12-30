2a8eb70aefb56732fbfaa59bd3134618fe808bfb
Alexandre Folgierini <95387509+afolgierini@users.noreply.github.com>
2022-12-30 12:44:49 -0300

    update from http to https on coordinate fetch

--- src/modules/coordinates.js
+++ src/modules/coordinates.js
@@ -2,7 +2,7 @@ import { getWeather } from "./weather"
 
 export const getCoordinates = async (location) => {   
     try {
-        let coordinates = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=4e32fe686f9174ced15dc7d9d5725636`)
+        let coordinates = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=4e32fe686f9174ced15dc7d9d5725636`)
         
         return await coordinates.json().then((res) => {
             document.querySelector('.location').innerText = res[0].name;
